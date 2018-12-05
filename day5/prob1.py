8a7e159fab85ba5e310530669b0f141886913e27
wohnlice <wohnlice@imsweb.com>
2018-12-05 16:22:49 -0500

    refactor polymer building

--- day5/prob1.py
+++ day5/prob1.py
@@ -6,20 +6,16 @@ def get_polymer():
 sample = get_polymer()
 
 
-def traverse(polymer):
-    idx = 0
-    while idx < len(polymer):
-        if idx + 1 == len(polymer):
-            return polymer
-        if polymer[idx] != polymer[idx + 1] and polymer[idx].lower() == polymer[idx + 1].lower():
-            polymer = polymer[:idx] + polymer[min(idx + 2, len(polymer)):]
-            if idx != 0:
-                idx -= 1  # go back a step if not already at the beginning. Previous unit might now catalyze
+def build_polymer(base_polymer):
+    polymer = []
+    for unit in base_polymer:
+        if polymer and unit != polymer[-1] and unit.lower() == polymer[-1].lower():
+            del polymer[-1]
         else:
-            idx += 1
-    return polymer
+            polymer.append(unit)
+    return ''.join(polymer)
 
 
 if __name__ == '__main__':
-    print traverse('dabAcCaCBAcCcaDA')  # control
-    print len(traverse(sample))
+    print build_polymer('dabAcCaCBAcCcaDA')  # control
+    print build_polymer('dabAcCaCBAcCcaDA')
--- day5/prob2.py
+++ day5/prob2.py
@@ -1,13 +1,14 @@
-from prob1 import get_polymer, traverse
+from prob1 import get_polymer, build_polymer
 
 
 def anomalous_unit():
     lowercase = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    base_polymer = get_polymer()
     modified = []
 
     for char in lowercase:
-        polymer = get_polymer().replace(char, '').replace(char.upper(), '')
-        modified.append(len(traverse(polymer)))
+        polymer = base_polymer.replace(char, '').replace(char.upper(), '')
+        modified.append(len(build_polymer(polymer)))
     letter = modified.index(min(modified)) + ord('a')  # don't actually need this, but for debug
     return min(modified)
 
