6d7691500f87fa71be32354d85f5bd3933d63805
wohnlice <wohnlice@imsweb.com>
2018-12-05 14:01:59 -0500

    prob2 for day4 and refactor

---
+++ day4/prob1.py
@@ -0,0 +1,45 @@
+import re
+
+def get_guards():
+    with open('source.txt') as source:
+        entries = source.readlines()
+        entries = sorted([entry.strip() for entry in entries])
+
+    guards = {}
+    for entry in entries:
+        timestamp, action = re.match(r'\[(.*)\] (.*)', entry).groups()
+        if action.startswith('Guard'):
+            curr_guard = re.match(r'(.*)#(\d+)', action).groups()[-1]
+        elif action == 'falls asleep':
+            start_time = timestamp[-2:]
+        elif action == 'wakes up':
+            end_time = timestamp[-2:]
+            duration = int(end_time) - int(start_time)
+            if curr_guard not in guards:
+                guards[curr_guard] = {'total': 0, 'ranges': []}
+            guards[curr_guard]['total'] += duration
+            guards[curr_guard]['ranges'].append(start_time + '-' + end_time)
+    return guards
+
+
+def get_max_guard(guards):
+    def get_total(guard):
+        return guard[1]['total']
+
+    return max(guards.items(), key=get_total)
+
+
+def get_guard_minutes(sleep_times):
+    minutes = [0] * 60
+    for sleep_range in sleep_times['ranges']:
+        start = int(sleep_range[:2])
+        end = int(sleep_range[3:])
+        for minute in range(start, end):
+            minutes[minute] += 1
+    return minutes
+
+
+guard_id, stimes = get_max_guard(get_guards())
+minutes = get_guard_minutes(stimes)
+max_minute = minutes.index(max(minutes))
+print int(guard_id) * max_minute
