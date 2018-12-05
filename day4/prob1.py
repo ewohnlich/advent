c97d9de6a7f36e81a8c40bdd427472d3ed177ef9
wohnlice <wohnlice@imsweb.com>
2018-12-05 14:03:14 -0500

    cleanup warning statements in pycharm

--- day4/prob1.py
+++ day4/prob1.py
@@ -1,11 +1,15 @@
 import re
 
+
 def get_guards():
     with open('source.txt') as source:
         entries = source.readlines()
         entries = sorted([entry.strip() for entry in entries])
 
     guards = {}
+    curr_guard = None
+    start_time = '0'
+
     for entry in entries:
         timestamp, action = re.match(r'\[(.*)\] (.*)', entry).groups()
         if action.startswith('Guard'):
@@ -30,13 +34,13 @@ def get_max_guard(guards):
 
 
 def get_guard_minutes(sleep_times):
-    minutes = [0] * 60
+    _minutes = [0] * 60
     for sleep_range in sleep_times['ranges']:
         start = int(sleep_range[:2])
         end = int(sleep_range[3:])
         for minute in range(start, end):
-            minutes[minute] += 1
-    return minutes
+            _minutes[minute] += 1
+    return _minutes
 
 
 guard_id, stimes = get_max_guard(get_guards())
